1a0c7ddc763a9661cba43dfaca391aa237e974a8
Naveen Jangir <206243@cognizant.com>
2022-05-02 15:00:01 +0530

    project load

---
+++ src/test/java/org/example/TestCases.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class TestCases {
+  private WebDriver driver;
+  private String url = "https://www.google.com/";
+
+  @BeforeTest
+  public void OpenChrome() {
+    WebDriverManager.chromedriver().setup();
+    driver = new ChromeDriver();
+  }
+
+  @Test
+  public void Case_001() {
+    driver.get(url);
+  }
+
+  @AfterTest
+  public void CloseBrowser() {
+    driver.close();
+  }
+}
