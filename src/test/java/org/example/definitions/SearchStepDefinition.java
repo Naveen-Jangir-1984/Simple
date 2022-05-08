8fff66be789754a84db4792a39a79a560e13c0de
Naveen Jangir <206243@cognizant.com>
2022-05-08 22:16:02 +0530

    updated gitignore and moved pages file.

--- src/test/java/org/example/definitions/SearchStepDefinition.java
+++ src/test/java/org/example/definitions/SearchStepDefinition.java
@@ -6,7 +6,6 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.example.HomePage;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
